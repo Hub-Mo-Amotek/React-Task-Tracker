aa23b2575d864956ff2f0f25e923a132c94c1918
Mo <Mohamed.elja@hotmail.com>
2024-02-05 16:23:45 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <div className="flex flex-col items-center m-8">
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">Task Tracker</h1>
+    </div>
+  )
+}
+
+export default App
