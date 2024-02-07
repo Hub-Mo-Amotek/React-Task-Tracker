85e6c2ecd9f6a1225d74cabe894c4d982e14d1f0
Mo <Mohamed.elja@hotmail.com>
2024-02-07 03:37:59 +0100

    Adds add task modal

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -11,13 +11,45 @@ const [addModal, setAddModal] = useState(false)
         {addModal ? (
             <>
             <div>
-                <div>
-                    <h3 className="flex items-center justify-center overflow-x-hidden overflow-y-auto fixed inset-0 z-100">
-                        MODAL
+                <div className="flex items-center justify-center overflow-x-hidden overflow-y-auto fixed inset-0 z-100">
+                    <div className="w-9/12 bg-white border rounded-lg shadow-md relative flex flex-col">
+                    <div className="flex flex-row justify-between p-5 border ng-white">
+                    <h3 className="bg-white text-3xl font-semibold">
+                        Add a task
                     </h3>
-                    <button className="px-1 text-gray-400 float-right text-3xl leading-none font-semibold block">
+                    <button className="px-1 text-gray-400 float-right text-3xl leading-none font-semibold block" 
+                            onClick={() => setAddModal(false)}>
                         x
                     </button>
+                    </div >
+                    <form className="p-6">
+                        <div>
+                            <label htmlFor="project-name" className="track-wide uppercase text-gray-700 text-xs font-semibold mb-2">
+                                Project name
+                            </label>
+                            <input type="text"
+                                className="w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 pc-4 mb-5 leading-tight focus:outline-none focus:bg-white"
+                                id="project-name"
+                                placeholder=" Project name"
+                                required
+                            />
+                        </div>
+                        <div>
+                            <label className="track-wide uppercase text-gray-700 text-xs font-semibold mb-2"  htmlFor="tas-description">Task description</label>
+                            <textarea 
+                                className="w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 pc-4 mb-5 leading-tight focus:outline-none focus:bg-white" 
+                                cols="30" 
+                                rows="10" 
+                                placeholder=" Task description "></textarea>
+                        </div>
+                    </form>
+                    <div className="flex justify-end m-2">
+                        <button
+                        className="bg-blue-500 text-white font-semibold uppercase text-sm px-6 py-3 rounded hover:opacity-70">
+                            Add task
+                        </button>
+                    </div>
+                    </div>
                 </div>
             </div>
             </>
